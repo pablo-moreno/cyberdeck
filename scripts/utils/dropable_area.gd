1cae7847386c4d71dab7d706a950266fb5a59dd4
Pablo Moreno <pablomoreno.inf@gmail.com>
2025-07-15 02:39:49 +0200

    feat: added drag and drop

--- scripts/utils/dropable_area.gd
+++ scripts/utils/dropable_area.gd
@@ -1,17 +1,19 @@
 class_name DropableCardArea extends Control
 
 
+func _get_targets():
+    return [get_parent()]
+
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
     if data is Card:
         if data.energy > GlobalSignals.energy_remaining:
             return false
-        
         return true
     return false
 
 func _drop_data(at_position: Vector2, card: Variant) -> void:
-    var parent = get_parent()
+    var targets = _get_targets()
     
     if card is Card:
-        card.play_card.emit([parent])
+        card.play_card.emit(targets)
