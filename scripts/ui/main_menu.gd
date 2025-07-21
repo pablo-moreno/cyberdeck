f3b4de043f4e18dd5aaad4c9d2a30f551bcaf200
Pablo Moreno <pablomoreno.inf@gmail.com>
2025-07-21 20:27:29 +0200

    feat: tweaks

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -3,8 +3,20 @@ class_name MainMenu extends PanelContainer
 
 var BATTLE_SCENE = preload("res://scenes/battle.tscn")
 
+@onready var scene_transition: SceneTransition = $SceneTransition
+
+func _ready() -> void:
+    scene_transition.visible = true
+    scene_transition.init_transition()
+    await scene_transition.animation_player.animation_finished
+    scene_transition.visible = false
+
 
 func _on_new_game_button_pressed() -> void:
+    scene_transition.visible = true
+    scene_transition.finish_transition()
+    await scene_transition.animation_player.animation_finished
+    scene_transition.visible = false
     get_tree().change_scene_to_file("res://scenes/battle.tscn")
 
 
